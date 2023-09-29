97d4e657aef6d5e87629b04687caebe56ccfe5e7
Harshit Saxena <harshits08@gmail.com>
2023-09-29 16:29:16 +0530

    Walks API Constants

--- src/common/constants/enums.ts
+++ src/common/constants/enums.ts
@@ -7,7 +7,10 @@ enum ExternalAPI {
     GET_ALL_CITIES = "/api/v1/zone/all",
     GET_CLOSEST_CITY = "/api/v1/zone/closest",
     GET_CITY_POI = "/api/v1/poi/path",
-    GET_ALL_POI = "/api/v1/poi/near"
+    GET_ALL_POI = "/api/v1/poi/near",
+    GET_ITENARARIES = "/api/v1/walk/near",
+    GET_ITENARARY_DETAILS = "/api/v1/walk/id"
+
 }
 
 export {PLATFORMS, ExternalAPI}
