40c9d1107d71b0124caf405b9b1d4f71f1dd35a8
Ishika Rastogi <ishika.rastogi@innobitsystems.com>
2023-10-01 12:03:45 +0530

    itinerary api change

--- src/modules/itenariesManagement/utils/externalComm.ts
+++ src/modules/itenariesManagement/utils/externalComm.ts
@@ -8,111 +8,119 @@ require('dotenv').config();
 
 class ItineraryWrapperService {
     private baseUrl: any
-    constructor(){
+    constructor() {
         this.baseUrl = process.env.ExtBaseURL;
-        this.getAllCities =this.getAllCities.bind(this);
+        this.getAllCities = this.getAllCities.bind(this);
         this.getClosestCity = this.getClosestCity.bind(this);
     }
 
-    async getAllCities (params?:any, query?:any) {
+    async getAllCities(params?: any, query?: any) {
         try {
-            let response = await axios.get(`${this.baseUrl}${ExternalAPI.GET_ALL_CITIES}`,{
+            let response = await axios.get(`${this.baseUrl}${ExternalAPI.GET_ALL_CITIES}`, {
                 headers: this.getReqHeaders()
             })
-            const {data} = response;
+            const { data } = response;
             return Promise.resolve(data)
-            
+
         } catch (error: any) {
-            const {response} = error;
-            if(response?.data){
+            const { response } = error;
+            if (response?.data) {
                 logger.error("Error in getting cities list [IineraryWrapperService]", response?.data)
-            return Promise.reject(response?.data)
+                return Promise.reject(response?.data)
             }
             logger.error("Error in getting cities list [IineraryWrapperService]", error)
             return Promise.reject(error)
         }
     }
 
-    async getClosestCity (params?:any, query?:any) {
+    async getClosestCity(params?: any, query?: any) {
         try {
-            const {lat, long} = query;
+            const { lat, long } = query;
 
-            if(!lat || !long){
+            if (!lat || !long) {
                 throw new Exception(ERROR_TYPE.BAD_REQUEST, "Lattitude and Longitude ar required.")
             }
-            let response = await axios.get(`${this.baseUrl}${ExternalAPI.GET_CLOSEST_CITY}`,{
+            let response = await axios.get(`${this.baseUrl}${ExternalAPI.GET_CLOSEST_CITY}`, {
                 headers: this.getReqHeaders(),
                 params: {
                     lat: lat,
                     lon: long
                 }
             })
-            const {data} = response;
+            const { data } = response;
             return Promise.resolve(data)
-            
-        } catch (error:any) {
-            const {response} = error;
-            if(response?.data){
+
+        } catch (error: any) {
+            const { response } = error;
+            if (response?.data) {
                 logger.error("Error in getting closest city [WrapperService]", response?.data)
-            return Promise.reject(response?.data)
+                return Promise.reject(response?.data)
             }
             logger.error("Error in getting closest city [WrapperService]", error)
             return Promise.reject(error)
         }
     }
 
-    async getItenararies (params?:any, query?:any) {
+    async getItenararies(params?: any, query?: any) {
         try {
-            const {city} = query;
+            const { city } = query;
 
-            let response = await axios.get(`${this.baseUrl}${ExternalAPI.GET_ITENARARIES}`,{
+            let response = await axios.get(`${this.baseUrl}${ExternalAPI.GET_ITENARARIES}`, {
                 headers: this.getReqHeaders(),
                 params: {
                     city
                 }
             })
-            const {data} = response;
+            const { data } = response;
             return Promise.resolve(data)
-            
-        } catch (error:any) {
-            const {response} = error;
-            if(response?.data){
+
+        } catch (error: any) {
+            const { response } = error;
+            if (response?.data) {
                 logger.error("Error in getting list of itenararies of city [WrapperService]", response?.data)
-            return Promise.reject(response?.data)
+                return Promise.reject(response?.data)
             }
             logger.error("Error in getting list of itenararies of city [WrapperService]", error)
             return Promise.reject(error)
         }
     }
 
-    async getItenararyDetails (params?:any, query?:any) {
+    async getItenararyDetails(params?: any, query?: any) {
         try {
-            const {walkId} = query;
-
-            let response = await axios.get(`${this.baseUrl}${ExternalAPI.GET_ITENARARY_DETAILS}`,{
+            const { walkId } = query;
+            var walksData:any = []
+            var poiData: any = []
+            let response = await axios.get(`${this.baseUrl}${ExternalAPI.GET_ITENARARY_DETAILS}`, {
                 headers: this.getReqHeaders(),
                 params: {
                     walkid: walkId
                 }
             })
-            const {data} = response;
-            return Promise.resolve(data)
-            
-        } catch (error:any) {
-            const {response} = error;
-            if(response?.data){
+            response.data.map((item: any) => {
+                if (item.seq != null) {
+                    poiData.push(item)
+                }else{
+                    walksData.push(item)
+                }
+            })
+            let result = {...{result1: poiData, result2: walksData}}
+            return Promise.resolve(result)
+
+        } catch (error: any) {
+            const { response } = error;
+            if (response?.data) {
                 logger.error("Error in getting details of itenarary [WrapperService]", response?.data)
-            return Promise.reject(response?.data)
+                return Promise.reject(response?.data)
             }
             logger.error("Error in getting details of itenarary [WrapperService]", error)
             return Promise.reject(error)
         }
     }
 
-    private getReqHeaders(){
+    private getReqHeaders() {
         let extApiKey = process.env.EXT_API_KEY;
         let headers = {
-            "api-key" : extApiKey
+            "api-key": extApiKey
         }
 
         return headers
